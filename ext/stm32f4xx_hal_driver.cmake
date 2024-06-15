7a320d7205d52509048bcbae79b7d35f7b9f0505
Jens Böckmann <jensboe@outlook.de>
2024-06-15 23:29:34 +0200

    Move HAL in a static lib

---
+++ ext/stm32f4xx_hal_driver.cmake
@@ -0,0 +1,42 @@
+
+
+project(stm32f4xx_hal_driver)
+
+add_library(stm32f4xx_hal_driver STATIC)
+
+target_compile_definitions(stm32f4xx_hal_driver PUBLIC 
+	STM32F446xx
+    $<$<CONFIG:Debug>:DEBUG>
+)
+
+target_include_directories(stm32f4xx_hal_driver PRIVATE
+    stm32f4xx_hal_driver/Inc
+    stm32f4xx_hal_driver/Inc/Legacy
+    ../Core/Inc/hal
+)
+
+target_sources(stm32f4xx_hal_driver PRIVATE
+    stm32f4xx_hal_driver/Src/stm32f4xx_hal_tim.c
+    stm32f4xx_hal_driver/Src/stm32f4xx_hal_tim_ex.c
+    stm32f4xx_hal_driver/Src/stm32f4xx_hal_uart.c
+    stm32f4xx_hal_driver/Src/stm32f4xx_hal_rcc.c
+    stm32f4xx_hal_driver/Src/stm32f4xx_hal_rcc_ex.c
+    stm32f4xx_hal_driver/Src/stm32f4xx_hal_flash.c
+    stm32f4xx_hal_driver/Src/stm32f4xx_hal_flash_ex.c
+    stm32f4xx_hal_driver/Src/stm32f4xx_hal_flash_ramfunc.c
+    stm32f4xx_hal_driver/Src/stm32f4xx_hal_gpio.c
+    stm32f4xx_hal_driver/Src/stm32f4xx_hal_dma_ex.c
+    stm32f4xx_hal_driver/Src/stm32f4xx_hal_dma.c
+    stm32f4xx_hal_driver/Src/stm32f4xx_hal_pwr.c
+    stm32f4xx_hal_driver/Src/stm32f4xx_hal_pwr_ex.c
+    stm32f4xx_hal_driver/Src/stm32f4xx_hal_cortex.c
+    stm32f4xx_hal_driver/Src/stm32f4xx_hal.c
+    stm32f4xx_hal_driver/Src/stm32f4xx_hal_exti.c
+    stm32f4xx_hal_driver/Src/stm32f4xx_hal_pcd.c
+    stm32f4xx_hal_driver/Src/stm32f4xx_hal_pcd_ex.c
+    stm32f4xx_hal_driver/Src/stm32f4xx_ll_usb.c
+)
+target_link_libraries(stm32f4xx_hal_driver PUBLIC
+    cmsis_5
+    cmsis_device_f4
+)
